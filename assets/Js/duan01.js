98f5acc9bbf3585dd546e2c07d1e68a8ac4f1e7c
Choucute2022 <chaucute2022@gmail.com>
2025-05-05 01:09:07 +0700

    Add files via upload

--- assets/Js/duan01.js
+++ assets/Js/duan01.js
@@ -1,1 +1,7 @@
-
+<script>
+  document.getElementById('loginButton').addEventListener('click', function() 
+    // Thực hiện các hành động khác nếu cần
+    // ...
+    window.location.href = "dangnhap.html";
+  );
+</script>
