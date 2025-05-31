61bfd579de899917b731845de2e642bb04fb39cf
Tieuphung0409 <phamtieuphung040906@gmail.com>
2025-05-31 08:08:08 +0700

    Delete Admin directory

--- Admin/Models/seed_database.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Hash mật khẩu cho admin
-$adminPassword = password_hash('admin123', PASSWORD_DEFAULT);
-echo "Mật khẩu hash cho admin (admin123): $adminPassword\n";
-
-// Hash mật khẩu cho user
-$userPassword = password_hash('user123', PASSWORD_DEFAULT);
-echo "Mật khẩu hash cho user (user123): $userPassword\n";
-?>
