5b29f957381a44f032ba0070c14c1fd9980e9969
Tieuphung0409 <phamtieuphung040906@gmail.com>
2025-05-31 07:57:58 +0700

    Delete duan01.js

--- duan01.js
+++
@@ -1,7 +0,0 @@
-<script>
-  document.getElementById('loginButton').addEventListener('click', function() 
-    // Thực hiện các hành động khác nếu cần
-    // ...
-    window.location.href = "dangnhap.html";
-  );
-</script>
